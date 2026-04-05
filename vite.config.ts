e8e9bde5d0ce20f15088a15ea96e8cf9a06f62c1
eduardofabian435-lgtm <eduardofabian435@gmail.com>
2026-04-05 07:20:23 -0300

    nova atualização

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: './',
+    base: '/Gestor-de-presen-a/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
